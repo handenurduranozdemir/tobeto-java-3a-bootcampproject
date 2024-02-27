070059325cbacb16c281bc787b4e9ef19180693b
handenurduranozdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-02-27 22:00:11 +0300

    handleDataResult & handleResult methodları BaseController sınıfına eklendi

--- src/main/java/com/tobeto/bootcampProject/webapi/BaseController.java
+++ src/main/java/com/tobeto/bootcampProject/webapi/BaseController.java
@@ -1,7 +1,25 @@
 package com.tobeto.bootcampProject.webapi;
 
-import com.tobeto.bootcampProject.business.concretes.ApplicantManager;
+import com.tobeto.bootcampProject.core.results.DataResult;
+import com.tobeto.bootcampProject.core.results.Result;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BaseController {
+    public ResponseEntity<?> handleDataResult(DataResult<?> dataResult)
+    {
+        if(dataResult.isSuccess()){
+            return ResponseEntity.ok(dataResult);
+        }
+        return ResponseEntity.badRequest().body(dataResult);
+    }
 
+    public ResponseEntity<?> handleResult(Result result)
+    {
+        if(result.isSuccess()){
+            return ResponseEntity.ok(result);
+        }
+        return ResponseEntity.badRequest().body(result);
+    }
 }
