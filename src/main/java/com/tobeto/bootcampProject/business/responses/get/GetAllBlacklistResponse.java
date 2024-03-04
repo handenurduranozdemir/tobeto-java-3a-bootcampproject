c7d4db13bda86ac5fae5d57e1931fefd6166ee7b
handenurduranozdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-03-04 03:06:17 +0300

    Response sınıflarına gerekli anotasyonlar eklendi

--- src/main/java/com/tobeto/bootcampProject/business/responses/get/GetAllBlacklistResponse.java
+++ src/main/java/com/tobeto/bootcampProject/business/responses/get/GetAllBlacklistResponse.java
@@ -1,7 +1,14 @@
 package com.tobeto.bootcampProject.business.responses.get;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAllBlacklistResponse {
     private int id;
     private String reason;
--- src/main/java/com/tobeto/bootcampProject/business/responses/get/GetAllBootcampStatesResponse.java
+++ src/main/java/com/tobeto/bootcampProject/business/responses/get/GetAllBootcampStatesResponse.java
@@ -1,5 +1,12 @@
 package com.tobeto.bootcampProject.business.responses.get;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAllBootcampStatesResponse {
     private int id;
     private String state;
