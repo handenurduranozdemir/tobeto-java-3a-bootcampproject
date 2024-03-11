75f8157ec561a82bcb46ddf459c508fc9cd9a21f
handenurduranozdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-03-11 03:09:46 +0300

    Create işlemi için validasyon kuralları eklendi

--- src/main/java/com/tobeto/bootcampProject/business/requests/create/CreateApplicantRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/create/CreateApplicantRequest.java
@@ -1,16 +1,47 @@
 package com.tobeto.bootcampProject.business.requests.create;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateApplicantRequest {
-    private String about;
-    private String userName;
+    @NotEmpty(message = "First name must not be empty")
+    @Size(min = 2, max = 50, message = "First name must be contain at least 2 characters")
     private String firstName;
+
+    @NotEmpty(message = "Last name must not be empty")
+    @Size(min = 2, max = 50, message = "Last name must be contain at least 2 characters")
     private String lastName;
+
+    @NotEmpty(message = "username must not be empty")
+    @Size(min = 2, max = 50, message = "Username must be contain at least 2 characters")
+    private String username;
+
+    @NotEmpty(message = "National identity must be not empty")
+    @Size(min = 11, max = 11, message = "National identity must be contain 11 characters")
     private String nationalIdentity;
+
+    @NotEmpty(message = "Email must not be empty")
+    @Email(message = "Invalid email address")
+    private String email;
+
+    @NotNull(message = "Date of Birt must not be null")
+    @Past(message = "Date of birth must be in the past")
+    private Date dateOfBirth;
+
+    @NotEmpty(message = "About must not be null")
+    private String about;
+
+    @NotEmpty(message = "Password must not be empty")
+    @Size(min = 8, max = 30, message = "Password must be contain at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d.+$)",
+            message = "Password must contain at least one lowercase character, " +
+                    "one uppercase character and one number character")
+    private String password;
 }
